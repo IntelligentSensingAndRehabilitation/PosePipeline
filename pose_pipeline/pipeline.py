855fc786ff77a876ea8f1489d81f70595a8f5967
Kunal Shah <kshah@sralab.org>
2024-01-09 22:51:23 +0000

    devcontainer: Adding mmpose to devcontainer
    In docker-compose for the devcontainer, allowing access to
    more GPUs (this will change in the future). Adding mmpose
    to the Dockerfile. Updating the mmdet wrapper, no functional
    changes. Updating the mmpose wrapper to be compatible with the
    new version of mmpose and adding COCO Wholebody RTMPose model.
    Updating pipeline.py to use the RTMPose COCO Wholebody model.
    Updating the getting started notebook to also run the mmpose
    steps. Needs to be cleaned up.

--- pose_pipeline/pipeline.py
+++ pose_pipeline/pipeline.py
@@ -493,7 +493,6 @@ class TrackingBboxMethodLookup(dj.Lookup):
         {"tracking_method": 7, "tracking_method_name": "MMTrack_qdtrack"},
         {"tracking_method": 8, "tracking_method_name": "MMDet_deepsort"},
         {"tracking_method": 9, "tracking_method_name": "MMDet_qdtrack"},
-
     ]
 
 
@@ -1010,6 +1009,7 @@ class TopDownMethodLookup(dj.Lookup):
         {"top_down_method": 12, "top_down_method_name": "Bridging_bml_movi_87"},
         {"top_down_method": 13, "top_down_method_name": "Bridging_smpl+head_30"},
         {"top_down_method": 14, "top_down_method_name": "Bridging_smplx_42"},
+        {"top_down_method": 15, "top_down_method_name": "MMPose_RTMPose_Coco_Wholebody"},
     ]
 
 
@@ -1095,6 +1095,9 @@ class TopDownPerson(dj.Computed):
             # Filter out keypoints that are outside of the image since confidence estimates do
             # not capture this
             key["keypoints"] = keypoints_filter_clipped_image(key, key["keypoints"])
+        elif method_name == "MMPose_RTMPose_Coco_Wholebody":
+            from .wrappers.mmpose import mmpose_top_down_person
+            key["keypoints"] = mmpose_top_down_person(key, "RTMPose_coco-wholebody")
         elif method_name == "Bridging_smplx_42":
             from pose_pipeline.wrappers.bridging import filter_skeleton
             from pose_pipeline.utils.keypoints import keypoints_filter_clipped_image
