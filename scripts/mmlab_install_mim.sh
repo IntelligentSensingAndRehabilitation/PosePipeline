41482036777e088d9021a1ad3c04eafa348953ff
Kunal Shah <kshah@sralab.org>
2025-06-06 15:32:05 -0500

    mmlab_install: Updating mmlab install script

--- scripts/mmlab_install_mim.sh
+++ scripts/mmlab_install_mim.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y build-essential
+pip install psutil ninja
+
 mim install mmengine
 mim install mmcv==2.1.0
-mim install mmpretrain==1.2.0
+mim install mmpretrain
 mim install mmdet==3.2.0
 mim install mmpose==1.3.2
