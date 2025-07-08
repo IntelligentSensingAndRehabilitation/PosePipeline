e0c56404abc75f0fb0d6bbe2ab8ef24579da689e
Kunal Shah <kshah@sralab.org>
2025-07-08 21:06:20 +0000

    mmpose: Adding mmpose tests

--- tests/test_mmlab_packages.py
+++ tests/test_mmlab_packages.py
@@ -3,6 +3,10 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
 
+# base path is relative to the location of this script
+base_path = os.path.dirname(__file__)
+
+
 def test_mmengine():
     import mmengine
 
@@ -58,41 +62,6 @@ def test_mmcv():
     print('Environment information:')
     print(dash_line + env_info + '\n' + dash_line)
 
-# def test_mmdetection():
-#     from mim import download 
-#     import mmdet.apis
-#     from mmdet.utils import register_all_modules
-
-#     os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
-
-#     package = 'mmdet'
-
-#     config_file = 'rtmdet_tiny_8xb32-300e_coco'
-#     checkpoint_file = 'rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth'
-
-#     # Define the destination folder
-#     destination = os.path.join(os.getcwd(), 'mmdetection/')
-
-#     # Download the model and checkpoints
-#     download(package, [config_file], dest_root=destination)
-
-#     # Define the model config and checkpoints paths
-#     model_config = os.path.join(destination, f"{config_file}.py")
-#     detector_checkpoint = os.path.join(destination, checkpoint_file)
-#     # Register all modules from mmdet
-#     register_all_modules()
-
-#     # Initialize the model
-#     model = mmdet.apis.init_detector(model_config, detector_checkpoint, device='cuda:0')
-#     # Perform inference
-#     result = mmdet.apis.inference_mot(model, 'demo/demo.jpg')
-
-#     # model = init_detector(config_file, checkpoint_file, device='cpu')
-#     # inference_detector(model, 'demo/demo.jpg')
-
-#     # model = init_detector(config_file, checkpoint_file, device='cuda:0')
-#     # inference_detector(model, 'demo/demo.jpg')
-
 def test_mmdetection():
     from mim import download 
     import mmdet.apis
@@ -105,11 +74,10 @@ def test_mmdetection():
     checkpoint_file = 'rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth'
 
     # Base path is relative to the location of this script
-    base_path = os.path.dirname(__file__)
-    input_image_path = os.path.join(base_path, "demo.jpg")
+    input_image_path = os.path.join(base_path, "det_demo.jpg")
 
     # Define the destination folder relative to the location of this script
-    destination = os.path.join(os.path.dirname(__file__), 'mmdetection/')
+    destination = os.path.join(base_path, 'mmdetection/')
 
     # Download the model and checkpoints
     download(package, [config_file], dest_root=destination)
@@ -128,15 +96,10 @@ def test_mmdetection():
 def test_mmdetection_demo():
 
     from mmengine.logging import print_log
-
     from mmdet.apis import DetInferencer
 
     # Base path is relative to the location of this script
-    base_path = os.path.dirname(__file__)
-
-    # cfg_path = "mmdetection/rtmdet_tiny_8xb32-300e_coco.py"
     cfg_path = os.path.join(base_path, "mmdetection/rtmdet_tiny_8xb32-300e_coco.py")
-    # ckpt_path = "mmdetection/rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth"
     ckpt_path = os.path.join(base_path, "mmdetection/rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth")
 
     inferencer = DetInferencer(
@@ -146,8 +109,8 @@ def test_mmdetection_demo():
         palette="none",
     )
 
-    input_image_path = os.path.join(base_path, "demo.jpg")
-    output_image_path = os.path.join(base_path, "test_output")
+    input_image_path = os.path.join(base_path, "det_demo.jpg")
+    output_image_path = os.path.join(base_path, "mmdet_output")
 
     # Run inference on the single image
     inferencer(
@@ -160,3 +123,84 @@ def test_mmdetection_demo():
         no_save_pred=False,
         print_result=True,
     )
+
+def test_mmpose():
+    from mim import download
+    from mmpose.apis import inference_topdown, init_model
+    from mmpose.utils import register_all_modules
+
+    # Register all modules from mmpose
+    register_all_modules()
+
+    package = 'mmpose'
+
+    # base path is relative to the location of this script
+    input_image_path = os.path.join(base_path, "det_demo.jpg")
+
+    # Define the model config and checkpoint files
+    pose_config_id = "td-hm_hrnet-w48_8xb32-210e_coco-256x192"
+    pose_checkpoint = "td-hm_hrnet-w48_8xb32-210e_coco-256x192-0e67c616_20220913.pth"
+
+    # define the destination folder
+    destination = os.path.join(base_path, f"mmpose/")
+
+    # download the model and checkpoints
+    download(package, [pose_config_id], dest_root=destination)
+
+    # define the model config and checkpoints paths
+    pose_cfg = os.path.join(destination, f"{pose_config_id}.py")
+    pose_ckpt = os.path.join(destination, pose_checkpoint)
+
+    # Initialize the model
+    pose_model = init_model(pose_cfg, pose_ckpt, device='cuda:0')
+
+    print(inference_topdown(pose_model, input_image_path))
+
+def test_mmpose_demo():
+    from mmengine.logging import print_log
+    from mmcv.image import imread
+    from mmpose.apis import init_model, inference_topdown
+    from mmpose.registry import VISUALIZERS
+    from mmpose.structures import merge_data_samples
+
+    # Base path is relative to the location of this script
+    cfg_path = os.path.join(base_path, "mmpose/td-hm_hrnet-w48_8xb32-210e_coco-256x192.py")
+    ckpt_path = os.path.join(base_path, "mmpose/td-hm_hrnet-w48_8xb32-210e_coco-256x192-0e67c616_20220913.pth")
+
+    model = init_model(
+        cfg_path,
+        ckpt_path,
+        device='cuda:0',
+        cfg_options=dict(model=dict(test_cfg=dict(output_heatmaps=True)))
+    )
+
+    input_image_path = os.path.join(base_path, "pose_demo.jpg")
+    output_image_path = os.path.join(base_path, "mmpose_output.jpg")
+
+    # init visualizer
+    model.cfg.visualizer.radius = 3
+    model.cfg.visualizer.alpha = 0.8
+    model.cfg.visualizer.line_width = 1
+
+    visualizer = VISUALIZERS.build(model.cfg.visualizer)
+    visualizer.set_dataset_meta(
+        model.dataset_meta, skeleton_style='mmpose')
+    
+    # inference a single image
+    batch_results = inference_topdown(model, input_image_path)
+    results = merge_data_samples(batch_results)
+
+    # show the results
+    img = imread(input_image_path, channel_order='rgb')
+    visualizer.add_datasample(
+        'result',
+        img,
+        data_sample=results,
+        draw_gt=False,
+        draw_bbox=True,
+        kpt_thr=0.3,
+        draw_heatmap=True,
+        show_kpt_idx=True,
+        skeleton_style='mmpose',
+        show=False,
+        out_file=output_image_path)
